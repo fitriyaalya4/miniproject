2a32f95b51f231abeb5ffb42e59ede76bd2cd06f
Fitriya Nur Alyashifa <fitriyaalyaa@gmail.com>
2025-06-13 01:39:55 +0700

    Membuat indikator loading JSON

--- app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
@@ -11,13 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,12 +64,24 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data by viewModel.data
-
-    LazyVerticalGrid(
-        modifier = modifier.fillMaxSize().padding(4.dp),
-        columns = GridCells.Fixed(2),
-    ) {
-        items(data){ ListItem(tanaman = it)}
+    val status by viewModel.status.collectAsState()
+    when (status) {
+        TanamanApi.ApiStatus.LOADING -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        TanamanApi.ApiStatus.SUCCESS -> {
+            LazyVerticalGrid(
+                modifier = modifier.fillMaxSize().padding(4.dp),
+                columns = GridCells.Fixed(2),
+            ) {
+                items(data) { ListItem(tanaman = it) }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainViewModel.kt
@@ -7,18 +7,23 @@ import androidx.lifecycle.viewModelScope
 import com.mobproassesment.miniproject3.model.Tanaman
 import com.mobproassesment.miniproject3.network.TanamanApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     var data = mutableStateOf(emptyList<Tanaman>())
+            private set
+    var status = MutableStateFlow(TanamanApi.ApiStatus.LOADING)
             private set
     init {
         retrieveData()
     }
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
+            status.value = TanamanApi.ApiStatus.LOADING
             try {
                 data.value = TanamanApi.service.getTanaman()
+                status.value = TanamanApi.ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
