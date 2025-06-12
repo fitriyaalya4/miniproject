dc92a88c07eebd3e7f3d57d438ddfbab1464b7f9
Fitriya Nur Alyashifa <fitriyaalyaa@gmail.com>
2025-06-13 04:01:12 +0700

    Membuat tampilan profil

--- app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
@@ -28,6 +28,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -66,6 +69,8 @@ fun MainScreen() {
     val context = LocalContext.current
     val dataStore = UserDataStore(context)
     val user by dataStore.userFlow.collectAsState(initial = User())
+
+    var showDialog by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -82,7 +87,7 @@ fun MainScreen() {
                             CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
                         }
                         else{
-                            Log.d("SIGN-IN","User: $user")
+                            showDialog = true
                         }
                     }) {
                         Icon(
@@ -96,6 +101,13 @@ fun MainScreen() {
         }
     ) { innerPadding ->
         ScreenContent(Modifier.padding(innerPadding))
+        if (showDialog){
+            ProfilDialog(
+                user = user,
+                onDismissRequest = { showDialog = false}) {
+                showDialog = false
+            }
+        }
     }
 }
 
