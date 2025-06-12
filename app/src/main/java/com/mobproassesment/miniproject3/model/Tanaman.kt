697d034fabde170c59b11d0d580db7b12c2e3d5f
Fitriya Nur Alyashifa <fitriyaalyaa@gmail.com>
2025-06-13 00:44:17 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/mobproassesment/miniproject3/model/Tanaman.kt
@@ -0,0 +1,9 @@
+package com.mobproassesment.miniproject3.model
+
+data class Tanaman(
+    val id: String,
+    val nama: String,
+    val gambar: String,
+    val mine: Int
+)
+
--- app/src/main/java/com/mobproassesment/miniproject3/network/TanamanApiAervice.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/network/TanamanApiAervice.kt
@@ -1,19 +1,25 @@
 package com.mobproassesment.miniproject3.network
 
+import com.mobproassesment.miniproject3.model.Tanaman
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://asessment3-api-production-c0f9.up.railway.app/api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface TanamanApiAervice {
     @GET("tanaman")
-    suspend fun getTanaman(): String
+    suspend fun getTanaman(): List<Tanaman>
 }
 
 object TanamanApi{
