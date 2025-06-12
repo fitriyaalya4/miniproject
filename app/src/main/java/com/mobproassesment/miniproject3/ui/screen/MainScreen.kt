a2112223140e48aad800bcbbca8fe9338e316014
Fitriya Nur Alyashifa <fitriyaalyaa@gmail.com>
2025-06-13 01:21:27 +0700

    Menangani gambar gagal di-load

--- app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -84,6 +85,8 @@ fun ListItem(tanaman: Tanaman) {
                 .build(),
             contentDescription = stringResource(R.string.gambar, tanaman.nama),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_img),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (modifier = Modifier.fillMaxWidth().padding(4.dp)
