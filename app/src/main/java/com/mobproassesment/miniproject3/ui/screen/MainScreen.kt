ada324985e8bd4f87bc8edb07dbc176332e1aa3c
Fitriya Nur Alyashifa <fitriyaalyaa@gmail.com>
2025-06-13 01:14:18 +0700

    Membuat tampilan nama hewan

--- app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mobproassesment/miniproject3/ui/screen/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.mobproassesment.miniproject3.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,11 +19,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -70,7 +74,8 @@ fun ListItem(tanaman: Tanaman) {
     Box(
         modifier = Modifier
             .padding(4.dp)
-            .border(1.dp, Color.Gray)
+            .border(1.dp, Color.Gray),
+            contentAlignment = Alignment.BottomCenter
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -81,6 +86,16 @@ fun ListItem(tanaman: Tanaman) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column (modifier = Modifier.fillMaxWidth().padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ){
+            Text(
+                text = tanaman.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
     }
 }
 @Preview(showBackground = true)
